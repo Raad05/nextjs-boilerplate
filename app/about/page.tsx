cbdfa17cc71020e3daac7d9762779a6cd63e11b9
Raad05 <ashabulraad@gmail.com>
2024-01-02 23:35:41 +0600

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <p>This is the about page</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import AddToCart from "./AddToCart";
+
+const ProductCard = () => {
+  return (
+    <div className="product-card">
+      <AddToCart></AddToCart>
+    </div>
+  );
+};
+
+export default ProductCard;
